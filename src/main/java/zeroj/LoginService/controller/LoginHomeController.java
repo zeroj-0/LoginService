6fe1071980033935428433515fec8d5568522aca
syj <syj990930@naver.com>
2021-09-09 15:19:15 +0900

    저장됨 ..

--- src/main/java/zeroj/LoginService/controller/LoginHomeController.java
+++ src/main/java/zeroj/LoginService/controller/LoginHomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import zeroj.LoginService.domain.Member;
+import zeroj.LoginService.repository.JpaRepository;
 
 @Controller
 public class LoginHomeController {
